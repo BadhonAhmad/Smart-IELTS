14637e7c74fc035cd6bba89c271c757137a32d0f
sk-sazid <1.sksazid@gmail.com>
2025-09-23 22:02:52 +0600

    update frontend

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Nobel\Desktop\Projects\Smart IELTS\frontend\src\app\layout.tsx
+// File: /home/sk-sazid/Desktop/Smart-IELTS/frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
